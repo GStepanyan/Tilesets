58cf3b092844b0775be23c7e69806510b62e3c21
Gor Stepanyan <gor.stepanyan@sflpro.com>
2021-01-31 16:24:56 +0400

    add

--- Tilesets/ViewController.swift
+++ Tilesets/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     }
 
     private func configureMapView() {
+        
+        
         mapView.delegate = self
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
